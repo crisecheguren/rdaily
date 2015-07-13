b2ecb16240d3b3e4edb2b747cbd8bde2ecd5cbb6
Cris Echeguren <Cris.Echeguren@gmail.com>
2015-07-13 12:38:04 -0700

    completed extra credit for challenge #1

--- Challenge 1/GetInfo.java
+++ Challenge 1/GetInfo.java
@@ -10,12 +10,16 @@ for extra credit, have the program log this information in a file to be accessed
 */
 
 import java.util.Scanner;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class GetInfo {
 	public static void main ( String[] args ) {
 		Scanner keyboard = new Scanner(System.in);
 
-		String name, username;
+		String name, username, info;
 		int age;
 
 		System.out.println( "What's your name?" );
@@ -27,7 +31,32 @@ public class GetInfo {
 		System.out.println( "What's your reddit username?" );
 		username = keyboard.next();
 
-		System.out.println( "Your name is " + name + ", you are " + age +" years old, and your username is " + username + "." );
+		info = ( "Your name is " + name + ", you are " + age +" years old, and your username is " + username + "." );
+
+		System.out.println(info);
+
+		try {
+
+			System.out.println("Writing to file...");
+
+			File file = new File("/users/coog/userInfo.txt");
+
+			// if file doesn't exists, then create it
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+
+
+			FileWriter fw = new FileWriter(file.getAbsoluteFile());
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write(info);
+			bw.close();
+
+			System.out.println("Done");
+
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
 
 	}
 }
