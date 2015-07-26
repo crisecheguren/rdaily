5f883392eb06b51337cd9520011428ee9a054145
Cris Echeguren <Cris.Echeguren@gmail.com>
2015-07-25 19:44:26 -0700

    completed challenge 2

---
+++ Challenge 2/PhysicsCalculator.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+public class PhysicsCalculator {
+
+	public static void main(String[] args) {
+		Scanner keyboard = new Scanner(System.in);
+
+
+
+		System.out.println("What are we solving for? (1-3)");
+		System.out.println("1) Force\n2) Mass\n3) Acceleration\n");
+		System.out.print(">   ");
+		int choice = keyboard.nextInt();
+
+		if (choice == 1) {
+			// F = m * a
+			System.out.println( fma() );
+		} else if (choice == 2) {
+			// m = F / a
+			System.out.println( mfa() );
+		} else if (choice == 3) {
+			// a = F / m
+			System.out.println( afm() );
+		} else {
+			// error message "wat do you mean?"
+			System.out.println("I don't know what you mean.");
+		}
+
+
+	}
+
+	public static double fma() {
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("What's the mass? (kg)");
+		System.out.print(">   ");
+		double m = keyboard.nextDouble();
+		System.out.println("What is the acceleration? (m/s squared)");
+		System.out.print(">   ");
+		double a = keyboard.nextDouble();
+
+		return m * a;
+	}
+
+	public static double mfa() {
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("What's the force? (N)");
+		System.out.print(">   ");
+		double f = keyboard.nextDouble();
+		System.out.println("What's the acceleration? (m/s squared)");
+		System.out.print(">   ");
+		double a = keyboard.nextDouble();
+
+		return f / a;
+	}
+
+	public static double afm() {
+		Scanner keyboard = new Scanner(System.in);
+		System.out.println("What's the force? (N)");
+		System.out.print(">   ");
+		double f = keyboard.nextDouble();
+		System.out.println("What's the mass? (kg)");
+		System.out.print(">   ");
+		double m = keyboard.nextDouble();
+
+		return f / m;
+	}
+
+}
